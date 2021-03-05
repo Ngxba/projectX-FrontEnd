12b27dd9edafe5fbcb87d2085d09a9db454b79a2
Tung Lam Nguyen Ba <tunglam.ngxba@gmail.com>
2021-03-05 14:18:49 +0700

    fix using useStyle

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,16 @@
-
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './theme';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment key="main">
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        HelloWorld
+      </ThemeProvider>
+    </React.Fragment>
   );
 }
 
