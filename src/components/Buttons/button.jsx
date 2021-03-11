2deb3c4f7f754c32e0269f72683c4b88406e438d
Hoang Nguyen <20176020@sie.edu.vn>
2021-03-11 15:13:16 +0700

    Fix typo

--- src/components/Buttons/button.jsx
+++ src/components/Buttons/button.jsx
@@ -56,7 +56,7 @@ CustomButton.propTypes = {
   children: PropTypes.node.isRequired,
   variant: PropTypes.oneOf(VARIANTS),
   backgroundColor: PropTypes.oneOf(BG_COLORS),
-  buttonSize: PropTypes.oneOf([SIZES]),
+  buttonSize: PropTypes.oneOf(SIZES),
 };
 
 export default CustomButton;
