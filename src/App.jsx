62085c93619ef4e5090d20cf2b1f3161fae654bc
Hoang Nguyen <20176020@sie.edu.vn>
2021-03-13 20:04:19 +0700

    remove redundant changes for font

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core';
 import CustomTheme from './theme';
-import ContentHeader from './container/content_header/content_header';
 
 function App()
 {
@@ -12,10 +11,6 @@ function App()
       <ThemeProvider theme={CustomTheme}>
 
         <CssBaseline />
-
-        <ContentHeader
-          bgImage="//stockx-assets.imgix.net/Core/homepage/hero_image_FW_2021.png?auto=compress,format"
-        />
 
       </ThemeProvider>
 
