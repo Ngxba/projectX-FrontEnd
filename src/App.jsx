34aca03f28d3e83af69d7a1a18b3d44ed79f0dc9
Hoang Nguyen <20176020@sie.edu.vn>
2021-03-18 15:22:09 +0700

    Remove redundant code in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,6 @@ import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core';
 import CustomTheme from './theme';
-import Footer from './components/Footer/footer';
-import FooterNews from './components/Footer/footer_news/footer_news';
-import newsData from './components/Footer/footer_news/data';
-import infoData from './components/Footer/site_information/data';
-import socialData from './components/Footer/social_medias/data';
-import SiteInfo from './components/Footer/site_information/site_information';
-import SocialMedias from './components/Footer/social_medias/social_medias';
 
 function App()
 {
@@ -18,12 +11,6 @@ function App()
       <ThemeProvider theme={CustomTheme}>
 
         <CssBaseline />
-
-        <Footer
-          FooterNews={<FooterNews newsData={newsData} />}
-          SiteInfo={<SiteInfo infoData={infoData} />}
-          SocialMedias={<SocialMedias socialData={socialData} />}
-        />
 
       </ThemeProvider>
 
