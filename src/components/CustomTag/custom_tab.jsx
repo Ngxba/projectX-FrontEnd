cf8239e9bfef8741c1f8e962a09981e9a49501a2
Hoang Nguyen <20176020@sie.edu.vn>
2021-03-25 23:05:49 +0700

    Add CustomTab

---
+++ src/components/CustomTag/custom_tab.jsx
@@ -0,0 +1,126 @@
+/* eslint-disable react/forbid-prop-types,react/jsx-props-no-spreading */
+import React from 'react';
+import PropTypes from 'prop-types';
+import SwipeableViews from 'react-swipeable-views';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+function TabPanel(props)
+{
+  const {
+    children,
+    value,
+    index,
+    ...other
+  } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box paddingBottom={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node.isRequired,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index)
+{
+  return {
+    id: `full-width-tab-${index}`,
+    'aria-controls': `full-width-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+const CustomTab = (props) =>
+{
+  const { labels, children } = props;
+
+  const classes = useStyles();
+  const theme = useTheme();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) =>
+  {
+    setValue(newValue);
+  };
+
+  const handleChangeIndex = (index) =>
+  {
+    setValue(index);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar
+        position="static"
+        color="default"
+        style={{
+          width: '500px',
+          backgroundColor: 'white',
+          boxShadow: 'none',
+          margin: '0 auto',
+        }}
+      >
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          variant="fullWidth"
+          aria-label="full width tabs example"
+          centered
+        >
+          {
+            labels.map((label, index) => (
+              <Tab label={label} {...a11yProps(index)} />
+            ))
+          }
+        </Tabs>
+      </AppBar>
+      <SwipeableViews
+        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        index={value}
+        onChangeIndex={handleChangeIndex}
+      >
+        {
+          children.map((child, index) => (
+            <TabPanel value={value} index={index} dir={theme.direction}>
+              {child}
+            </TabPanel>
+          ))
+        }
+      </SwipeableViews>
+    </div>
+  );
+};
+
+CustomTab.propTypes = {
+  children: PropTypes.arrayOf(PropTypes.node).isRequired,
+  labels: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default CustomTab;
