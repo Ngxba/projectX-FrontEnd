0d54965c2c8611ad7989e78a2b026ed0eb3ae02d
Hoang Nguyen <20176020@sie.edu.vn>
2021-03-27 15:43:24 +0700

    Modify to make use of CustomTab

--- src/container/authen/authen.jsx
+++ src/container/authen/authen.jsx
@@ -1,112 +1,54 @@
 /* eslint-disable react/forbid-prop-types */
-import React from "react";
-import PropTypes from "prop-types";
-import SwipeableViews from "react-swipeable-views";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Tabs from "@material-ui/core/Tabs";
-import Tab from "@material-ui/core/Tab";
-import Typography from "@material-ui/core/Typography";
-import Box from "@material-ui/core/Box";
-import Paper from "@material-ui/core/Paper";
-import Login from "./login";
-import Register from "./register";
+import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import { PropTypes } from 'prop-types';
+import Login from './login';
+import Register from './register';
+import CustomTab from '../../components/CustomTab/custom_tab';
 
-function TabPanel(props) {
-  const {
-    children, value, index, ...other
-  } = props;
+const Wrapper = ({ children }) => (
+  <>
+    <Paper style={{ width: '400px', padding: '0 16px', margin: '0 auto' }}>
+      {children}
+    </Paper>
+  </>
+);
 
-  return (
-    <div
-      role='tabpanel'
-      hidden={value !== index}
-      id={`full-width-tabpanel-${index}`}
-      aria-labelledby={`full-width-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box paddingBottom={3}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-TabPanel.propTypes = {
+Wrapper.propTypes = {
   children: PropTypes.node.isRequired,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
 };
 
-function a11yProps(index) {
-  return {
-    id: `full-width-tab-${index}`,
-    "aria-controls": `full-width-tabpanel-${index}`,
-  };
-}
-
-const useStyles = makeStyles({
-  root: {
-    backgroundColor: "#fafafa",
-    width: "100vw",
-    height: "100vh",
-    paddingTop: "15vh",
+const data = [
+  {
+    title: 'Log in',
+    component: <Login />,
   },
-});
+  {
+    title: 'Sign up',
+    component: <Register />,
+  },
+];
 
-export default function FullWidthTabs() {
-  const classes = useStyles();
-  const theme = useTheme();
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
-  const handleChangeIndex = (index) => {
-    setValue(index);
-  };
-
-  return (
-    <div className={classes.root}>
-      <Paper style={{ width: "400px", padding: "0 16px", margin: "0 auto" }}>
-        <AppBar
-          position='static'
-          color='default'
-          style={{
-            width: "100%",
-            backgroundColor: "white",
-            boxShadow: "none",
-          }}
-        >
-          <Tabs
-            value={value}
-            onChange={handleChange}
-            indicatorColor='primary'
-            textColor='secondary'
-            variant='fullWidth'
-            aria-label='full width tabs example'
-            centered
-          >
-            <Tab label='Log in' {...a11yProps(0)} />
-            <Tab label='Sign up' {...a11yProps(1)} />
-          </Tabs>
-        </AppBar>
-        <SwipeableViews
-          axis={theme.direction === "rtl" ? "x-reverse" : "x"}
-          index={value}
-          onChangeIndex={handleChangeIndex}
-        >
-          <TabPanel value={value} index={0} dir={theme.direction}>
-            <Login />
-          </TabPanel>
-          <TabPanel value={value} index={1} dir={theme.direction}>
-            <Register />
-          </TabPanel>
-        </SwipeableViews>
-      </Paper>
-    </div>
-  );
+const LoginRegisterForm = () => (
+  <CustomTab
+    data={data}
+    appBarStyle={
+      {
+        width: '100%',
+        backgroundColor: 'white',
+        boxShadow: 'none',
+      }
 }
+    wrapper={Wrapper}
+    rootStyle={
+      {
+        backgroundColor: '#fafafa',
+        width: '100vw',
+        height: '100vh',
+        paddingTop: '15vh',
+      }
+    }
+  />
+);
+
+export default LoginRegisterForm;
