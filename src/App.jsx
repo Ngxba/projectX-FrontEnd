30e9f52a0050a1d61d5ccc326b58a5f8d969229b
Hoang Nguyen <20176020@sie.edu.vn>
2021-03-27 15:44:32 +0700

    Demo LoginRegisterForm and CarouselWithTab

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,10 @@ import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core';
 import CustomTheme from './theme';
-import CustomTab from './components/CustomTab/custom_tab';
-import CustomCarousel from './container/carousel/carousel';
+import LoginRegisterForm from './container/authen/authen';
+import CarouselWithTab from './container/tab_carousel/tab_carousel';
 
 // import LoginScreen from './container/Login/login';
-
-const carousels = [
-  {
-    title: 'Item one',
-    component: <CustomCarousel />,
-  },
-  {
-    title: 'Item two',
-    component: <CustomCarousel />,
-  },
-  {
-    title: 'Item three',
-    component: <CustomCarousel />,
-  },
-
-];
 
 function App()
 {
@@ -32,12 +16,9 @@ function App()
 
         <CssBaseline />
 
-        <CustomTab
-          labels={['Item one', 'Item two', 'Item three']}
-          data={carousels}
-        />
+        <LoginRegisterForm />
 
-        {/* <LoginScreen /> */}
+        <CarouselWithTab />
 
       </ThemeProvider>
 
