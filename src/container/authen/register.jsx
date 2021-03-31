d61edf67675dde6310c370a91f6bb92ad55deef3
Tung Lam Nguyen Ba <tunglam.ngxba@gmail.com>
2021-03-31 10:00:59 +0700

    authen with backend

--- src/container/authen/register.jsx
+++ src/container/authen/register.jsx
@@ -1,122 +1,144 @@
 /* eslint-disable react/jsx-wrap-multilines */
-import React from 'react';
-import { TextField, FormControlLabel } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import IconButton from '@material-ui/core/IconButton';
-import OutlinedInput from '@material-ui/core/OutlinedInput';
-import InputLabel from '@material-ui/core/InputLabel';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import FormControl from '@material-ui/core/FormControl';
-import Visibility from '@material-ui/icons/Visibility';
-import VisibilityOff from '@material-ui/icons/VisibilityOff';
-import Checkbox from '@material-ui/core/Checkbox';
-import CustomTypography from '../../components/Typography/typography';
-import CustomButton from '../../components/Buttons/button';
+import React from "react";
+import {
+  TextField,
+  FormControlLabel,
+  CircularProgress,
+} from "@material-ui/core";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import Alert from '@material-ui/lab/Alert';
+import IconButton from "@material-ui/core/IconButton";
+import OutlinedInput from "@material-ui/core/OutlinedInput";
+import InputLabel from "@material-ui/core/InputLabel";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import FormControl from "@material-ui/core/FormControl";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
+import Checkbox from "@material-ui/core/Checkbox";
+import CustomTypography from "../../components/Typography/typography";
+import CustomButton from "../../components/Buttons/button";
+import { isLengthEqualZero, validateEmail } from "../../utils/supportFunction";
 
 const styles = makeStyles({
   root: {
-    width: '100%',
-    marginBottom: '16px',
-    '& label': {
-      fontSize: '14px',
+    width: "100%",
+    marginBottom: "16px",
+    "& label": {
+      fontSize: "14px",
     },
-    '& .MuiInputLabel-outlined.MuiInputLabel-shrink': {
-      transform: 'translate(14px, 3px) scale(0.75)',
-      color: 'rgba(0, 0, 0, 0.54)',
+    "& .MuiInputLabel-outlined.MuiInputLabel-shrink": {
+      transform: "translate(14px, 3px) scale(0.75)",
+      color: "rgba(0, 0, 0, 0.54)",
     },
-    '& legend': {
-      display: 'none',
+    "& legend": {
+      display: "none",
     },
   },
   main: {
-    backgroundColor: '#fafafa',
-    width: '100vw',
-    height: '100vh',
+    backgroundColor: "#fafafa",
+    width: "100vw",
+    height: "100vh",
   },
 });
 
-function Register()
-{
+function Register({ submit, status }) {
   const classes = styles();
   const [values, setValues] = React.useState({
-    password: '',
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
     showPassword: false,
     approveTerm: false,
+    loading: false,
   });
-  const handleClickShowPassword = () =>
-  {
+  const handleClickShowPassword = () => {
     setValues({ ...values, showPassword: !values.showPassword });
   };
 
-  const handleClickTerm = () =>
-  {
+  const handleClickTerm = () => {
     setValues({ ...values, approveTerm: !values.approveTerm });
   };
 
-  const handleChange = (prop) => (event) =>
-  {
+  const handleChange = (prop) => (event) => {
     setValues({ ...values, [prop]: event.target.value });
   };
 
-  const handleMouseDownPassword = (event) =>
-  {
+  const handleMouseDownPassword = (event) => {
     event.preventDefault();
   };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setValues({ ...values, loading: true });
+    await submit(values);
+    setValues({ ...values, loading: false });
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
+      {status === 2 && (
+        <Alert variant='outlined' severity='error' style={{ backgroundColor: "#fff3f3", marginTop: "20px" }}>
+          Account email already assigned! Please try again!
+        </Alert>
+      )}
       <br />
       <TextField
         className={classes.root}
-        id="outlined-basic"
-        color="secondary"
-        label="First Name"
-        variant="outlined"
+        color='secondary'
+        label='First Name'
+        variant='outlined'
+        name="firstName"
+        onChange={handleChange("firstName")}
       />
       <TextField
         className={classes.root}
-        id="outlined-basic"
-        color="secondary"
-        label="Last Name"
-        variant="outlined"
+        color='secondary'
+        label='Last Name'
+        name="lastName"
+        variant='outlined'
+        onChange={handleChange("lastName")}
       />
       <TextField
         className={classes.root}
-        id="outlined-basic"
-        color="secondary"
-        label="Email Address"
-        variant="outlined"
+        color='secondary'
+        label='Email Address'
+        variant='outlined'
+        name="email"
+        onChange={handleChange("email")}
       />
       <FormControl
         className={classes.root}
-        variant="outlined"
-        color="secondary"
+        variant='outlined'
+        color='secondary'
       >
-        <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+        <InputLabel htmlFor='outlined-adornment-password'>Password</InputLabel>
         <OutlinedInput
-          id="outlined-adornment-password"
-          type={values.showPassword ? 'text' : 'password'}
+          id='outlined-adornment-password'
+          type={values.showPassword ? "text" : "password"}
           value={values.password}
-          onChange={handleChange('password')}
+          name="password"
+          onChange={handleChange("password")}
           endAdornment={
-            <InputAdornment position="end">
+            <InputAdornment position='end'>
               <IconButton
-                aria-label="toggle password visibility"
+                aria-label='toggle password visibility'
                 onClick={handleClickShowPassword}
                 onMouseDown={handleMouseDownPassword}
-                edge="end"
+                edge='end'
               >
                 {values.showPassword ? <Visibility /> : <VisibilityOff />}
               </IconButton>
             </InputAdornment>
           }
           labelWidth={70}
-          style={{ marginBottom: '6px' }}
+          style={{ marginBottom: "6px" }}
         />
         <CustomTypography
-          fontSize="12px"
-          txtType="text--light"
-          txtComponent="p"
-          color="black"
+          fontSize='12px'
+          txtType='text--light'
+          txtComponent='p'
+          color='black'
         >
           At least 8 characters, 1 uppercase letter, 1 number & 1 symbol
         </CustomTypography>
@@ -126,33 +148,42 @@ function Register()
           <Checkbox
             checked={values.approveTerm}
             onChange={handleClickTerm}
-            name="jason"
+            name='jason'
           />
         }
-        style={{ cursor: 'default' }}
+        style={{ cursor: "default" }}
         label={
           <CustomTypography
-            fontSize="12px"
-            color="rgba(0, 0, 0, 0.54)"
-            txtType="text--light"
-            txtComponent="p"
+            fontSize='12px'
+            color='rgba(0, 0, 0, 0.54)'
+            txtType='text--light'
+            txtComponent='p'
           >
-            {'By signing up, you agree to the '}
-            <a style={{ color: 'rgba(0, 0, 0)' }} href="https://google.com">
+            {"By signing up, you agree to the "}
+            <a style={{ color: "rgba(0, 0, 0)" }} href='https://google.com'>
               Terms of Service
             </a>
-            {' and '}
-            <a style={{ color: 'rgba(0, 0, 0)' }} href="https://google.com">
+            {" and "}
+            <a style={{ color: "rgba(0, 0, 0)" }} href='https://google.com'>
               Privacy Policy
             </a>
           </CustomTypography>
         }
       />
-      <CustomButton style={{ width: '100%', margin: 0 }} type="submit">
-        Sign Up
+      <CustomButton style={{ width: "100%", margin: 0 }} disabled={values.loading || !values.approveTerm || isLengthEqualZero(values) || !validateEmail(values.email)} type='submit'>
+        {!values.loading ? (
+          "Sign Up"
+        ) : (
+          <CircularProgress color='secondary' size='20px' />
+        )}
       </CustomButton>
     </form>
   );
 }
+
+Register.propTypes = {
+  submit: PropTypes.func.isRequired,
+  status: PropTypes.number.isRequired,
+};
 
 export default Register;
---
+++ src/template/brand/brand.jsx
@@ -0,0 +1,139 @@
+import React from "react";
+import Container from "@material-ui/core/Container";
+import {
+  FormControl, Select, MenuItem, Grid,
+} from "@material-ui/core";
+// import { PropTypes } from 'prop-types';
+import CustomTypography from "../../components/Typography/typography";
+import CardContainer from "../../container/card_container/card_container";
+import Breadcrumb from "../../components/Breadcrumbs/breadcrumbs";
+import brandStyle from "./brand.style";
+
+const fakeData = [
+  {
+    name: "Product Title",
+    price: 100,
+    numberSold: 0,
+    imgSrc:
+      "https://stockx-360.imgix.net/Nike-Dunk-Low-Retro-White-Black-2021/Images/Nike-Dunk-Low-Retro-White-Black-2021/Lv2/img01.jpg?auto=compress&q=90&dpr=2&updated_at=1611084516&fit=clip&fm=webp&ixlib=react-9.0.3&w=1946",
+  },
+  {
+    name: "Product Title",
+    price: 100,
+    numberSold: 0,
+    imgSrc:
+      "https://stockx-360.imgix.net/Nike-Dunk-Low-Retro-White-Black-2021/Images/Nike-Dunk-Low-Retro-White-Black-2021/Lv2/img01.jpg?auto=compress&q=90&dpr=2&updated_at=1611084516&fit=clip&fm=webp&ixlib=react-9.0.3&w=1946",
+  },
+  {
+    name: "Product Title",
+    price: 100,
+    numberSold: 0,
+    imgSrc:
+      "https://stockx-360.imgix.net/Nike-Dunk-Low-Retro-White-Black-2021/Images/Nike-Dunk-Low-Retro-White-Black-2021/Lv2/img01.jpg?auto=compress&q=90&dpr=2&updated_at=1611084516&fit=clip&fm=webp&ixlib=react-9.0.3&w=1946",
+  },
+  {
+    name: "Product Title",
+    price: 100,
+    numberSold: 0,
+    imgSrc:
+      "https://stockx-360.imgix.net/Nike-Dunk-Low-Retro-White-Black-2021/Images/Nike-Dunk-Low-Retro-White-Black-2021/Lv2/img01.jpg?auto=compress&q=90&dpr=2&updated_at=1611084516&fit=clip&fm=webp&ixlib=react-9.0.3&w=1946",
+  },
+  {
+    name: "Product Title",
+    price: 100,
+    numberSold: 0,
+    imgSrc:
+      "https://stockx-360.imgix.net/Nike-Dunk-Low-Retro-White-Black-2021/Images/Nike-Dunk-Low-Retro-White-Black-2021/Lv2/img01.jpg?auto=compress&q=90&dpr=2&updated_at=1611084516&fit=clip&fm=webp&ixlib=react-9.0.3&w=1946",
+  },
+  {
+    name: "Product Title",
+    price: 100,
+    numberSold: 0,
+    imgSrc:
+      "https://stockx-360.imgix.net/Nike-Dunk-Low-Retro-White-Black-2021/Images/Nike-Dunk-Low-Retro-White-Black-2021/Lv2/img01.jpg?auto=compress&q=90&dpr=2&updated_at=1611084516&fit=clip&fm=webp&ixlib=react-9.0.3&w=1946",
+  },
+  {
+    name: "Product Title",
+    price: 100,
+    numberSold: 0,
+    imgSrc:
+      "https://stockx-360.imgix.net/Nike-Dunk-Low-Retro-White-Black-2021/Images/Nike-Dunk-Low-Retro-White-Black-2021/Lv2/img01.jpg?auto=compress&q=90&dpr=2&updated_at=1611084516&fit=clip&fm=webp&ixlib=react-9.0.3&w=1946",
+  },
+  {
+    name: "Product Title",
+    price: 100,
+    numberSold: 0,
+    imgSrc:
+      "https://stockx-360.imgix.net/Nike-Dunk-Low-Retro-White-Black-2021/Images/Nike-Dunk-Low-Retro-White-Black-2021/Lv2/img01.jpg?auto=compress&q=90&dpr=2&updated_at=1611084516&fit=clip&fm=webp&ixlib=react-9.0.3&w=1946",
+  },
+  {
+    name: "Product Title",
+    price: 100,
+    numberSold: 0,
+    imgSrc:
+      "https://stockx-360.imgix.net/Nike-Dunk-Low-Retro-White-Black-2021/Images/Nike-Dunk-Low-Retro-White-Black-2021/Lv2/img01.jpg?auto=compress&q=90&dpr=2&updated_at=1611084516&fit=clip&fm=webp&ixlib=react-9.0.3&w=1946",
+  },
+];
+
+const Brand = () => {
+  const classes = brandStyle();
+  const [age, setAge] = React.useState(10);
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  return (
+    <Container maxWidth='md' style={{ marginTop: "90px" }}>
+      <Grid className={classes.root} container xs={12}>
+        <CustomTypography component='h2' txtStyle='text--heading'>
+          Supreme
+        </CustomTypography>
+        <CustomTypography
+          component='p'
+          txtType='text--light'
+          style={{ maxWidth: "450px" }}
+        >
+          Buy and sell tees, hoodies, accessories and more from streetwear
+          juggernaught Supreme on StockX here!
+        </CustomTypography>
+      </Grid>
+      <Grid className={classes.gridContainer} container spacing={2}>
+        <Grid className={classes.leftSide} item xs={2}>
+          <div>Other Brands</div>
+        </Grid>
+        <Grid className={classes.rightSide} item xs={10}>
+          <Grid item xs={12} className={classes.sort}>
+            <Grid item xs={6}>
+              <Breadcrumb data={["home", "streetwear", "jordan"]} />
+            </Grid>
+            <Grid item xs={6} className={classes.formControl}>
+              <FormControl>
+                <div className={classes.package}>
+                  <div className={classes.textRow}>
+                    <span className={classes.textTitle}>
+                      <strong>Sort by:&#160;</strong>
+                    </span>
+                    <Select
+                      value={age}
+                      onChange={handleChange}
+                      className={classes.selectEmpty}
+                    >
+                      <MenuItem value={10}>Ten</MenuItem>
+                      <MenuItem value={20}>Twenty</MenuItem>
+                      <MenuItem value={30}>Thirty</MenuItem>
+                    </Select>
+                  </div>
+                </div>
+              </FormControl>
+            </Grid>
+          </Grid>
+          <Grid item xs={12}>
+            <CardContainer data={fakeData} showCategory={false} />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+export default Brand;
