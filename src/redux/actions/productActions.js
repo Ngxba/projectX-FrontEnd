5c6edb39d169af83e990b1d35ef5019e36be11bb
Hoang Nguyen <20176020@sie.edu.vn>
2021-04-17 15:21:07 +0700

    Fix Eslint

--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -1,87 +1,110 @@
 /* eslint-disable arrow-body-style */
 /* eslint-disable */
-import axios from "axios";
-import { backEndLink } from "../../config";
+import axios from 'axios';
+import { backEndLink } from '../../config';
 import {
   FETCH_PRODUCT_REQUEST,
   FETCH_PRODUCT_REQUEST_FAILURE,
   FETCH_PRODUCT_REQUEST_SUCCESS,
   FETCH_PRODUCTS_REQUEST,
   FETCH_PRODUCTS_REQUEST_FAILURE,
-  FETCH_PRODUCTS_REQUEST_SUCCESS,
-} from "../types/productType";
+  FETCH_PRODUCTS_REQUEST_SUCCESS
+} from '../types/productType';
 
 const FetchProductsRequest = () => ({
-  type: FETCH_PRODUCTS_REQUEST,
+  type: FETCH_PRODUCTS_REQUEST
 });
 
 const FetchProductsRequestSuccess = (products) => ({
   type: FETCH_PRODUCTS_REQUEST_SUCCESS,
-  payload: products,
+  payload: products
 });
 
 const FetchProductsRequestFailure = (error) => ({
   type: FETCH_PRODUCTS_REQUEST_FAILURE,
-  payload: error,
+  payload: error
 });
 
-export const FetchProducts = (offset = null, limit = null, params) => {
-  return async function (dispatch) {
+export const FetchProducts = (offset = null, limit = null, params) =>
+{
+  return async function (dispatch)
+  {
     dispatch(FetchProductsRequest());
     const {
-      category, tag, tag2, tag3, tag4, tag5,
+      category,
+      tag,
+      tag2,
+      tag3,
+      tag4,
+      tag5
     } = params;
     const tagQuery = [tag, tag2, tag3, tag4, tag5].reduce(
       (pre, cur, index) => (cur != null ? (pre += (index != 0 ? `,${cur}` : `${cur}`)) : pre),
-      tag != null ? "&tags=" : "",
+      tag != null ? '&tags=' : ''
     );
-    try {
+    try
+    {
       const res = await axios.get(`${backEndLink}/api/product/browse?offset=${offset}&limit=${limit}&productCategory=${category}${tagQuery}`);
-      if (res.status === 200) {
+      if (res.status === 200)
+      {
         dispatch(FetchProductsRequestSuccess(res.data.result));
-        let listBrand = []
-        for (let i = 0; i < res.data.result.length; i++){
-          if (!listBrand.includes(res.data.result[i].brand)) {
-            listBrand.push(res.data.result[i].brand)
+        let listBrand = [];
+        for (let i = 0; i < res.data.result.length; i++)
+        {
+          if (!listBrand.includes(res.data.result[i].brand))
+          {
+            listBrand.push(res.data.result[i].brand);
           }
         }
-        console.log(listBrand)
-      } else {
+        console.log(listBrand);
+      }
+      else
+      {
         //   throw new Error("Cannot Sign In", res.data.error);
         dispatch(FetchProductsRequestFailure(res));
       }
-    } catch (error) {
+    }
+    catch (error)
+    {
       dispatch(FetchProductsRequestFailure(error));
     }
   };
 };
 
 const FetchProductRequest = () => ({
-  type: FETCH_PRODUCT_REQUEST,
+  type: FETCH_PRODUCT_REQUEST
 });
 
 const FetchProductRequestSuccess = (products) => ({
   type: FETCH_PRODUCT_REQUEST_SUCCESS,
-  payload: products,
+  payload: products
 });
 
 const FetchProductRequestFailure = (error) => ({
   type: FETCH_PRODUCT_REQUEST_FAILURE,
-  payload: error,
+  payload: error
 });
 
-export const FetchProduct = (urlKey) => {
-  return async function (dispatch) {
+export const FetchProduct = (urlKey) =>
+{
+  return async function (dispatch)
+  {
     dispatch(FetchProductRequest());
-    try {
+    try
+    {
       const res = await axios.get(`${backEndLink}/api/product/${urlKey}`);
-      if (res.status === 200) {
+      if (res.status === 200)
+      {
         dispatch(FetchProductRequestSuccess(res.data.product));
-      } else {
+      }
+      else
+      {
         //   throw new Error("Cannot Sign In", res.data.error);
         dispatch(FetchProductsRequestFailure(res));
       }
-    } catch (error) {
+    }
+    catch (error)
+    {
       dispatch(FetchProductRequestFailure(error));
     }
   };
