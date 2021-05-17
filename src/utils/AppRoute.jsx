31fbe94e8e2c9645cb5e7e4fe3f0f8cd7324b165
Hoang Nguyen <20176020@sie.edu.vn>
2021-05-18 02:09:16 +0700

    Add private route for routes that require logged in

--- src/utils/AppRoute.jsx
+++ src/utils/AppRoute.jsx
@@ -1,33 +1,49 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Redirect, Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
-// import LayoutDefault from "../template/layout";
+import { useSelector } from 'react-redux';
 
 const AppRoute = ({
   component: Component,
   layout: Layout,
   isMainPage,
+  isPrivate,
   ...rest
-}) => (
-  <Route
-    {...rest}
-    render={(props) => (
-      <Layout isMainPage={isMainPage}>
-        <Component {...props} />
-      </Layout>
-    )}
-  />
-);
+}) =>
+{
+  const userState = useSelector((state) => state.userState);
+
+  const privateComponent = (props) => (
+    userState.isLogin
+      ? <Component {...props} />
+      : <Redirect push to="/login" />
+  );
+
+  return (
+    <Route
+      {...rest}
+      render={(props) => (
+        <Layout isMainPage={isMainPage}>
+          {isPrivate
+            ? privateComponent(props)
+            : <Component {...props} />}
+        </Layout>
+      )}
+    />
+  );
+};
 
 AppRoute.propTypes = {
   component: PropTypes.func.isRequired,
   layout: PropTypes.func,
   isMainPage: PropTypes.bool,
+  isPrivate: PropTypes.bool,
 };
 
 AppRoute.defaultProps = {
   layout: React.Fragment,
   isMainPage: false,
+  isPrivate: false,
 };
 
 export default AppRoute;
