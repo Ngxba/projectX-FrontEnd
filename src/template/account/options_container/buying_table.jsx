3942a0f633cde266b68e5aada2f4cf66f50c4193
Hoang Nguyen <20176020@sie.edu.vn>
2021-06-04 00:40:12 +0700

    Fix warning for duplicate key

--- src/template/account/options_container/buying_table.jsx
+++ src/template/account/options_container/buying_table.jsx
@@ -276,7 +276,7 @@ export default function EnhancedTable()
                       role="checkbox"
                       aria-checked={isItemSelected}
                       tabIndex={-1}
-                      key={row.name}
+                      key={row.productId}
                       selected={isItemSelected}
                     >
                       <TableCell padding="checkbox">
