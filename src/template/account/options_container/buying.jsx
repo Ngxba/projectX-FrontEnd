d6b877c4c75b77a3916ffb3c74bab39d851356e5
Hoang Nguyen <20176020@sie.edu.vn>
2021-06-04 21:04:05 +0700

    Convert from CustomTable to DataGrid

--- src/template/account/options_container/buying.jsx
+++ src/template/account/options_container/buying.jsx
@@ -1,9 +1,37 @@
+/* eslint-disable */
 import React from 'react';
 import { useSelector } from 'react-redux';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import buyingContainerStyle from './buying.style';
 import Shadow from '../../../components/Shadow/shadow';
-import CustomTable from './buying_table';
+import { DataGrid } from '@material-ui/data-grid';
+
+const columns = [
+  {
+    field: 'productName',
+    headerName: 'Product name',
+    flex: 0.5
+  },
+  {
+    field: 'status',
+    headerName: 'Status',
+    flex: 0.3
+  },
+  {
+    field: 'purchaseDate',
+    headerName: 'Purchase date',
+    type: 'dateTime',
+    flex: 0.5,
+    valueFormatter: (params) => params.value.toUTCString(),
+
+  },
+  {
+    field: 'price',
+    type: 'number',
+    headerName: 'Price',
+    flex: 0.3
+  }
+];
 
 const BuyingContainer = () =>
 {
@@ -11,6 +39,19 @@ const BuyingContainer = () =>
 
   const userState = useSelector((state) => state.userState);
   const orderState = useSelector((state) => state.orderState);
+
+  const rows = orderState.orderData.map((order, index) =>
+  {
+    return {
+      id: index + 1,
+      productName: order.productName,
+      status: order.status,
+      purchaseDate: new Date(order.purchaseDate),
+      price: order.price,
+    };
+  });
+
+  console.log(rows);
 
   if (userState.loading || orderState.loading)
   {
@@ -21,20 +62,34 @@ const BuyingContainer = () =>
           height: '100%',
           display: 'flex',
           justifyContent: 'center',
-          alignItems: 'center',
+          alignItems: 'center'
         }}
       >
-        <CircularProgress size={80} />
+        <CircularProgress size={80}/>
       </div>
     );
   }
 
   return (
     <>
-      <Shadow />
+      <Shadow/>
       <div className={classes.root}>
-        <CustomTable data={orderState.orderData} />
-      </div>
+        <div style={{ height: 850, width: '100%' }}>
+          <div style={{ display: 'flex', height: '100%' }}>
+            <div style={{ flexGrow: 1 }}>
+              <DataGrid
+                sortModel={[{
+                  field: 'purchaseDate',
+                  sort: 'desc'
+                }]}
+                rows={rows}
+                columns={columns}/>
+            </div>
+            </div>
+          </div>
+        </div>
+        {/*<CustomTable data={orderState.orderData}/>*/}
+
     </>
   );
 };
